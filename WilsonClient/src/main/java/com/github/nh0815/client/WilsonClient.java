c00ae6ff17a4dcec832c58ee95ee7379e6e04a04
Nick Hirakawa <nhirakawa@hubspot.com>
2016-09-14 13:11:54 -0400

    Reduce to skeleton

--- WilsonClient/src/main/java/com/github/nh0815/client/WilsonClient.java
+++ WilsonClient/src/main/java/com/github/nh0815/client/WilsonClient.java
@@ -1,16 +1,6 @@
 package com.github.nh0815.client;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.UnknownHostException;
-
 public class WilsonClient implements Runnable {
-
-  private static final String MULTICAST_ADDRESS = "224.0.0.1";
-  private static final int MULTICAST_PORT = 11224;
-  private static final int MAX_PACKET_SIZE = 65507;
 
   public WilsonClient() {
 
@@ -18,22 +8,7 @@ public class WilsonClient implements Runnable {
 
   @Override
   public void run() {
-    try {
-      InetAddress address = InetAddress.getByName(MULTICAST_ADDRESS);
-      try (MulticastSocket socket = new MulticastSocket(MULTICAST_PORT)) {
-        socket.joinGroup(address);
-        while (true) {
-          byte[] buffer = new byte[MAX_PACKET_SIZE];
-          DatagramPacket packet = new DatagramPacket(buffer, MAX_PACKET_SIZE);
-          socket.receive(packet);
-          System.out.println(new String(packet.getData(), 0, packet.getLength()));
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    }
+
   }
 
   public static void main(String... args) {
--- WilsonServer/src/main/java/com/github/nh0815/server/WilsonServer.java
+++ WilsonServer/src/main/java/com/github/nh0815/server/WilsonServer.java
@@ -1,17 +1,6 @@
 package com.github.nh0815.server;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
 public class WilsonServer implements Runnable {
-
-  private static final String MULTICAST_ADDRESS = "224.0.0.1";
-  private static final int MULTICAST_PORT = 11224;
 
   public WilsonServer() {
 
@@ -19,26 +8,11 @@ public class WilsonServer implements Runnable {
 
   @Override
   public void run() {
-    try {
-      InetAddress address = InetAddress.getByName(MULTICAST_ADDRESS);
-      try (DatagramSocket socket = new DatagramSocket()) {
-        System.out.printf("Listening on %d%n", MULTICAST_PORT);
-        while (true) {
-          System.out.println("Sending data...");
-          byte[] data = UUID.randomUUID().toString().getBytes();
-          DatagramPacket packet = new DatagramPacket(data, data.length, address, MULTICAST_PORT);
-          socket.send(packet);
-          Thread.sleep(TimeUnit.SECONDS.toMillis(1));
-        }
-      } catch (IOException | InterruptedException e) {
-        e.printStackTrace();
-      }
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    }
+
   }
 
-  public static void main(String... args) {
+  public static void main(String... args) throws Exception {
     new WilsonServer().run();
   }
+
 }
