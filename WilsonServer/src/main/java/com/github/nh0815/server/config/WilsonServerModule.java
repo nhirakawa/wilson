9f3322684ec07535f5850aa8056c2f72379a0693
Nick Hirakawa <nhirakawa@hubspot.com>
2016-10-26 22:53:05 -0400

    Provide singleton ObjectMapper in server module

--- WilsonServer/src/main/java/com/github/nh0815/server/config/WilsonServerModule.java
+++ WilsonServer/src/main/java/com/github/nh0815/server/config/WilsonServerModule.java
@@ -1,12 +1,21 @@
 package com.github.nh0815.server.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class WilsonServerModule extends AbstractModule {
 
   @Override
   protected void configure() {
 
+  }
+
+  @Provides
+  @Singleton
+  public ObjectMapper provideObjectMapper() {
+    return new ObjectMapper();
   }
 
 }
