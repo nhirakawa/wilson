901b762167fd7f4f20d8941034e55986e6d4a697
Nick Hirakawa <nhirakawa@hubspot.com>
2016-10-26 22:13:47 -0400

    Remove WilsonCoreModule

--- WilsonServer/src/main/java/com/github/nh0815/server/config/WilsonServerModule.java
+++ WilsonServer/src/main/java/com/github/nh0815/server/config/WilsonServerModule.java
@@ -1,13 +1,12 @@
 package com.github.nh0815.server.config;
 
-import com.github.nhirakawa.wilson.models.config.WilsonCoreModule;
 import com.google.inject.AbstractModule;
 
 public class WilsonServerModule extends AbstractModule {
 
   @Override
   protected void configure() {
-    install(new WilsonCoreModule());
+
   }
 
 }
